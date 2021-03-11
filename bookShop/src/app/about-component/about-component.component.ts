dc31e529c62f127fee61f7052202df2d2364015d
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2021-03-11 19:04:57 +0300

    feat: inject all services in about component

--- bookShop/src/app/about-component/about-component.component.ts
+++ bookShop/src/app/about-component/about-component.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Optional } from '@angular/core';
 import { GeneratorService } from '../services/generator.service';
+import { LocalStorageService } from '../services/local-storage.service';
+import { BooksService } from '../services/books.service';
+import { ConfigOptionsService } from '../services/config-options.service';
+import { CartService } from '../services/cart.service';
+import { ConstantsService } from '../services/constants.service';
 
 @Component({
   selector: 'app-about-component',
@@ -9,7 +14,7 @@ import { GeneratorService } from '../services/generator.service';
 export class AboutComponentComponent implements OnInit {
   rval = this.generatorService.GeneratorFactory(5);
 
-  constructor(@Optional() private generatorService: GeneratorService) { }
+  constructor(@Optional() private generatorService: GeneratorService, private localStorageService: LocalStorageService, private booksService: BooksService, private confugOptionService: ConfigOptionsService, private cartService: CartService, private constantsService: ConstantsService) { }
 
   ngOnInit(): void {
   }
