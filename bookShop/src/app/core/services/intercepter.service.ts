38ff7f5f5a5acd5e277c4c7208cba4ea6bb564c7
Konstantin Kravtsov <KostyaEK2006@gmail.com>
2021-05-05 20:45:45 +0300

    feat: add app-settings

--- bookShop/src/app/core/services/intercepter.service.ts
+++ bookShop/src/app/core/services/intercepter.service.ts
@@ -1,6 +1,14 @@
-import { HttpHandler, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { IBook } from 'src/app/shared/models/BookModel';
 
 @Injectable({
   providedIn: 'root',
@@ -8,11 +16,16 @@ import { map } from 'rxjs/operators';
 export class IntercepterService {
   constructor() {}
 
-  timingIntecepter(req: HttpRequest<unknown>, next: HttpHandler) {
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     const start = Date.now();
     return next.handle(req).pipe(
-      map((res) => {
-        console.log('took ' + (Date.now() - start) + 'ms');
+      map((res: HttpEvent<any>) => {
+        if (res instanceof HttpResponse && res.url?.match(/books\//)) {
+          console.log('took ' + (Date.now() - start) + 'ms');
+        }
         return res;
       })
     );
